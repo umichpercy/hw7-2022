5c00735b12cebf505dc1165a11c30693d37e8655
umichpercy <122488506+umichpercy@users.noreply.github.com>
2023-03-30 13:40:15 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -12,6 +12,9 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play()
+	console.log(video.volume)
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
+
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
@@ -37,24 +40,27 @@ document.querySelector("#faster").addEventListener("click", function() {
 document.querySelector("#skip").addEventListener("click", function() {
 	console.log("skip");
 	console.log(video.currentTime)
-	video.currentTime += 15;
+
+	if((video.currentTime + 15) < video.duration){
+		video.currentTime += 15;
+	}
+	else{
+		video.currentTime = 0
+	};
 	console.log(video.currentTime)
 });
 
 
-// var video = document.getElementById("myVideo");
-// video.onpause = function() {
-// 	console.log("The video has been paused");
-// };
-
-
-// var vid = document.getElementById("myVideo");
-// vid.volume = 0.2;
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("pause");
+	console.log(this.value)
+});
 
 document.querySelector("#slider").addEventListener("change", function() {
 	console.log("slider");
 	console.log(this.value)
 	video.volume = this.value / 100
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
 });
 
 
@@ -70,3 +76,13 @@ document.querySelector("#mute").addEventListener("click", function() {
 	}
 });
 
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("vintage");
+	video.setAttribute("class", "oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("orig");
+	video.removeAttribute("class");
+});
